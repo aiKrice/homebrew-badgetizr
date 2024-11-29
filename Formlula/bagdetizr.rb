8b2f848102f2f7d5ab8d4c82fffd16aca7b9e2b2
Chris Saez <saez.chris@gmail.com>
2024-11-29 21:50:14 +0100

    Test homebrew

--- Formlula/bagdetizr.rb
+++ Formlula/bagdetizr.rb
@@ -1,8 +1,8 @@
 class Badgetizr < Formula
     desc "Badgetizr is a tool to allow custom badges automatically added and updated according the content of your pull request."
     homepage "https://github.com/aiKrice/homebrew-badgetizr"
-    url "https://github.com/aiKrice/homebrew-badgetizr/archive/refs/tags/1.1.0.tar.gz"
-    sha256 "6c4657acc7ddf957079888887c8fde6db4d914636ce1053c6751c1f75aeff773"
+    url "https://github.com/aiKrice/homebrew-badgetizr/archive/refs/tags/1.1.1.tar.gz"
+    sha256 "42b29df7ee939ce597bb75d8c175d647fc15ca4f42c25d19ee2add5d328fb63c"
     license "MIT"
   
     depends_on "yq"
