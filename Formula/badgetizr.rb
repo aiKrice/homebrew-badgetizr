3c10ffc63e90d89bb11efc478a083e2637ba51a2
Chris Saez <saez.chris@gmail.com>
2024-11-30 10:10:56 +0100

    Update sha256

--- Formula/badgetizr.rb
+++ Formula/badgetizr.rb
@@ -2,7 +2,7 @@ class Badgetizr < Formula
     desc "Badgetizr is a tool to allow custom badges automatically added and updated according the content of your pull request."
     homepage "https://github.com/aiKrice/homebrew-badgetizr"
     url "https://github.com/aiKrice/homebrew-badgetizr/archive/refs/tags/1.1.3.tar.gz"
-    sha256 "75a2b06a34aac9ceade8da317f5cc10a2432f6d4d015b3248d1e5f2d5dde5e57"
+    sha256 "7161e53d6b455d824fa8f622dacd89a4bdedc98f90f8798de2ff62cabc92a951"
     license "MIT"
   
     depends_on "yq"
