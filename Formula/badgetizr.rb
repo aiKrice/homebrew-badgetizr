dd1736d8384a22a8f9c6eb9e46cbaa0a98249991
Chris Saez <saez.chris@gmail.com>
2025-09-14 18:44:01 +0200

    Bump version 1.6.0

--- Formula/badgetizr.rb
+++ Formula/badgetizr.rb
@@ -1,9 +1,9 @@
 class Badgetizr < Formula
     desc "Badgetizr is a tool to allow custom badges automatically added and updated according the content of your pull request."
     homepage "https://github.com/aiKrice/homebrew-badgetizr"
-    url "https://github.com/aiKrice/homebrew-badgetizr/archive/refs/tags/1.5.5.tar.gz"
+    url "https://github.com/aiKrice/homebrew-badgetizr/archive/refs/tags/1.6.0.tar.gz"
     head "https://github.com/aiKrice/homebrew-badgetizr.git", branch: "master"
-    sha256 "149cb0deb7c6d2ee3b83be5c555139b8bcf35d20c380efa120ebb291994cea38"
+    sha256 "c6beb40a6f0f33b4dd17f5d12be4bc0ef981bff6c0dfb7ce4c731f6210a8602e"
     license "MIT"
 
     depends_on "yq"
