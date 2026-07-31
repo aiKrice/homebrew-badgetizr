a0dbf60de9c6c0b6ca15b54ee31d0a6004fcbdb3
Chris Saez <saez.chris@gmail.com>
2026-07-31 18:08:01 +0200

    Bump version to 3.1.1 for -v option

--- step.sh
+++ step.sh
@@ -37,7 +37,7 @@ if [[ -z "${github_token}" ]] && [[ -z "${gitlab_token}" ]]; then
 fi
 
 # Badgetizr version (matches step version)
-BADGETIZR_VERSION="3.1.0"
+BADGETIZR_VERSION="3.1.1"
 
 echo "📦 Badgetizr version: ${BADGETIZR_VERSION}"
 
